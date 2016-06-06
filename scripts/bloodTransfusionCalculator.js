5d539b9de3850915f20191c4de90ee3fe26d8c01
AndyChen <shinningstar1001@gmail.com>
2016-06-06 23:53:41 +0800

    update cat and dog

--- scripts/bloodTransfusionCalculator.js
+++ scripts/bloodTransfusionCalculator.js
@@ -1,18 +1,19 @@
 angular
 .module('acToolsApp', ['ui.bootstrap'])
 .controller('calculatorCtrl', ['$scope', function($scope){
-	$scope.dog = {
-		weight : 0,
-		recipientPCV : 0,
-		donorPCV : 0,
-		desiredPCV : 0,
+	$scope.dog = { ratio : 90};
+	$scope.cat = { ratio : 60};
+	$scope.getTransfusionVolume = getTransfusionVolume;
+	$scope.getPCV = getPCV;
+
+	function getTransfusionVolume (animal) {
+		var result = animal.ratio*animal.weight*(animal.desiredPCV-animal.recipientPCV)/animal.donorPCV;
+		return !isNaN(result) ? result : 0;
 	}
 
-	$scope.getDogTransfusionVolume = getDogTransfusionVolume;
-
-	function getDogTransfusionVolume (dog) {
-
-		var result = 90*dog.weight*(dog.desiredPCV-dog.recipientPCV)/dog.donorPCV;
-		return angular.isNumber(result) && !isNaN(result) ? result : 0;
+	function getPCV (animal) {		
+		var result = animal.transfusionVolume*animal.donorPCV/animal.ratio/animal.weight + animal.recipientPCV;
+		return !isNaN(result) ? result : 0;
 	}
+
 }]);
